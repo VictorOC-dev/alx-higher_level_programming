1367bd69446b596298188b345d498b17a23528fe
VictorOC-dev <iyamavic9@gmail.com>
2023-12-06 02:10:40 -0800

    add correction

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     from add_0 import add
-    c = 1
-    d = 2
-    print("{} + {} = {}".format(c, d, add(c, d)))
+    a = 1
+    b = 2
+    print("{} + {} = {}".format(a, b, add(a, b)))
